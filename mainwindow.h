66fd9ac40a33d65c667c529dc778938ea1053059
iantv <ian.tatiana.v@gmail.com>
2016-11-21 20:55:17 +1000

    Use QUdpSocket Add code for QUdpSocket to constructor MainWindow::MainWindow() In comments method SendMessage()

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork/QUdpSocket>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+   // void SendMessage();
+private slots:
+    void on_sendButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QUdpSocket *socket;
+    qint32 port;
 };
 
 #endif // MAINWINDOW_H
