5f7b17535f0734cbaa8c509ffa6fde3b7a91c7d8
iantv <ian.tatiana.v@gmail.com>
2016-11-22 18:24:47 +1000

    Connect to socket and send test message.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,36 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+using namespace std;
+
+#define IP_ADDR "127.0.0.1"
+#define PORT 8000
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)/*, socket(new QUdpSocket(this)) */{
+    ui(new Ui::MainWindow){
     ui->setupUi(this);
-    port = 8000;
-    //socket = new QUdpSocket(this);
-    /*socket->bind(QHostAddress::LocalHost, port);
-
-    connect(socket, SIGNAL(readyRead()), SLOT(read()));*/
+    socket = new QUdpSocket(this);
+    socket->bind(QHostAddress(IP_ADDR), PORT);
+    connect(socket, SIGNAL(readyRead()), this, SLOT(ReadyRead()));
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
-/*
-void MainWindow::SendMessage() {
-    //void send(QString str, qint8 type) {
-    QString str = "Hello, robot!";
-    qint8 type = 0;
-    QByteArray data;
-    QDataStream out(&data, QIODevice::WriteOnly);
-    out << qint64(0);
-    out << qint8(type);
-    out << str;
-    out.device()->seek(qint64(0));
-    out << qint64(data.size() - sizeof(qint64));
-    socket->writeDatagram(data, QHostAddress::LocalHost, port);
-}*/
 
-void MainWindow::on_sendButton_clicked()
-{
-    //SendMessage();
+void MainWindow::SendMessage() {
+    QByteArray Data;
+    Data.append("Hello, robot!");
+    socket->writeDatagram(Data, QHostAddress(IP_ADDR), PORT);
+}
+
+void MainWindow::ReadyRead(){
+    QByteArray buffer;
+    buffer.resize(socket->pendingDatagramSize());
+
+    QHostAddress sender;
+    quint16 senderPort;
+
+    socket->readDatagram(buffer.data(), buffer.size(), &sender, &senderPort);
+    qDebug() << "Message from: " << sender.toString();
+    qDebug() << "Message port: " << senderPort;
+    qDebug() << "Message: " << buffer;
+}
+
+void MainWindow::on_sendButton_clicked() {
+    SendMessage();
 }
