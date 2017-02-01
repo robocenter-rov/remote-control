ac54ba5958610313108e80f02910b385486b60e0
iantv <ian.tatiana.v@gmail.com>
2017-02-01 15:34:27 +1000

    Replace creating QPen for drawing

--- figure.cpp
+++ figure.cpp
@@ -2,14 +2,14 @@
 
 Figure::Figure()
 {
-
+    _pen = QPen(QColor(0, 0, 127, 127));
 }
 
-LineFigure::LineFigure(QPointF p1, QPointF p2)
+LineFigure::LineFigure(QPointF p1, QPointF p2) :
+    Figure()
 {
     _p1 = p1;
     _p2 = p2;
-    _pen = QPen(QColor(0, 0, 127, 127));
 }
 
 void LineFigure::draw(GraphicsScene *scene)
