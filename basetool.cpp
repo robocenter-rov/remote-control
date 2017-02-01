34e0191c74e5ea055a1fd6ee71b6d00042a2aa79
iantv <ian.tatiana.v@gmail.com>
2017-02-01 11:54:38 +1000

    Check intersection drawing line with SceneRect sides

--- basetool.cpp
+++ basetool.cpp
@@ -3,6 +3,37 @@
 #include <QCoreApplication>
 
 #include "figure.h"
+#define SCENE_WIDTH 640
+#define SCENE_HEIGHT 480
+
+static bool inRect(QPointF p)
+{
+    return (p.x() >= 0 && p.x() <= SCENE_WIDTH && p.y() >= 0 && p.y() <= SCENE_HEIGHT);
+}
+
+static void intersection(QPointF &p1, QPointF &p2)
+{
+    QPointF points[4] = {QPointF(0, 0), QPointF(0, SCENE_HEIGHT), QPointF(SCENE_WIDTH, 0), QPointF(SCENE_WIDTH, SCENE_HEIGHT)};
+    QPointF t(0, 0);
+    for (int i = 0; i < 4; i++) {
+         if (((p1.x() < points[i].x()) && (p2.x() > points[i].x())) ||
+             ((p2.x() < points[i].x()) && (p1.x() > points[i].x()))){
+             t.setX(points[i].x());
+             t.setY((points[i].x() - p1.x())*(p2.y() - p1.y())/(p2.x() - p1.x())+p1.y());
+             if (inRect(p1) && !inRect(p2)) { p2.setX(t.x()); p2.setY((t.y())); }
+             if (inRect(p2) && !inRect(p1)) { p1.setX(t.x()); p1.setY((t.y())); }
+         }
+
+         if (((p1.y() < points[i].y()) && (p2.y() > points[i].y())) ||
+             ((p2.y() < points[i].y()) && (p1.y() > points[i].y()))){
+             t.setY(points[i].y());
+             t.setX((points[i].y() - p1.y())*(p2.x() - p1.x())/(p2.y() - p1.y())+p1.x());
+             if (inRect(p1) && !inRect(p2)) { p2.setX(t.x()); p2.setY((t.y())); }
+             if (inRect(p2) && !inRect(p1)) { p1.setX(t.x()); p1.setY((t.y())); }
+         }
+    }
+    qDebug() << p2.x() << " " << p2.y();
+}
 
 BaseTool::BaseTool(QWidget *parent)
 {
@@ -46,12 +77,15 @@ void LineTool::drawOnMousePress(GraphicsScene *scene, QPointF point)
 
 void LineTool::drawOnMouseMove(GraphicsScene *scene, QPointF point)
 {
-    scene->addLine(_startPos.x(), _startPos.y(), point.x(), point.y(), QPen(QColor(255, 0, 0, 127)));
+    _endPos = point;
+    intersection(_startPos, _endPos);
+    scene->addLine(_startPos.x(), _startPos.y(), _endPos.x(), _endPos.y(), QPen(QColor(255, 0, 0, 127)));
 }
 
 void LineTool::drawOnMouseRelease(GraphicsScene *scene, QPointF point)
 {
     _endPos = point;
+    intersection(_startPos, _endPos);
     if (_endPos != _startPos){
         scene->addFigure(new LineFigure(_startPos, _endPos));
     }
