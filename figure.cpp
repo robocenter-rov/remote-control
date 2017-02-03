cf6490c1ab7d27d4510d4b91e6ce6ed1ec33daf1
iantv <ian.tatiana.v@gmail.com>
2017-02-04 01:13:44 +1000

    Add methods to LineFigure for selection figure and calc selected area

--- figure.cpp
+++ figure.cpp
@@ -1,4 +1,5 @@
 #include "figure.h"
+#include <QtCore>
 
 Figure::Figure()
 {
@@ -8,13 +9,62 @@ Figure::Figure()
 LineFigure::LineFigure(QPointF p1, QPointF p2) :
     Figure()
 {
-    _p1 = p1;
-    _p2 = p2;
+    _p1 = (p1.x() < p2.x()) ? p1 : p2;
+    _p2 = (p1.x() < p2.x()) ? p2 : p1;
+    double deltay = _p2.y() - _p1.y();
+    double deltax = _p2.x() - _p1.x();
+    _angle = atan(deltay/deltax);
+    calcArea();
 }
 
 void LineFigure::draw(GraphicsScene *scene)
 {
     scene->addLine(_p1.x(), _p1.y(), _p2.x(), _p2.y(), _pen);
+    if (!_area.isEmpty()) {
+        scene->addPolygon(_area, QPen(QColor(127, 0, 127, 127)));
+    }
+}
+#include <math.h>
+static QPointF rotate(QPointF p, double angle)
+{
+    return QPointF(p.x()*cos(angle) - p.y()*sin(angle), p.x()*sin(angle) + p.y()*cos(angle));
+}
+
+bool LineFigure::inArea(QPointF p)
+{
+    calcArea();
+    QPointF t = rotate(QPointF(p.x() - _p1.x(), p.y() - _p1.y()), -_angle);
+    if (((t.x() > 0) &&
+         (t.x() < rotatedEndPoint2().x())) &&
+        ((t.y() > - _offset) &&
+         (t.y() < _offset)))
+        return true;
+    return false;
+}
+
+QPointF LineFigure::rotatedEndPoint2()
+{
+    return rotate(QPointF(_p2.x() - _p1.x(), _p2.y() - _p1.y()), -_angle);
+}
+
+void LineFigure::calcArea()
+{
+    QPointF np2 = rotatedEndPoint2();
+
+    QPointF p1t = rotate(QPointF(0, -_offset), _angle); // p1t point1top
+    QPointF p1b = rotate(QPointF(0, _offset), _angle);  // p1b point1bottom
+    QPointF p2t = rotate(QPointF(np2.x(), -_offset), _angle);
+    QPointF p2b = rotate(QPointF(np2.x(), _offset), _angle);
+
+    QPointF r1t(_p1.x() + p1t.x(), _p1.y() + p1t.y()); // result1top
+    QPointF r1b(_p1.x() + p1b.x(), _p1.y() + p1b.y()); // result1bottom
+    QPointF r2t(_p1.x() + p2t.x(), _p1.y() + p2t.y()); // ...
+    QPointF r2b(_p1.x() + p2b.x(), _p1.y() + p2b.y()); // ...
+
+    QVector <QPointF> points;
+    points << r1t << r2t << r2b << r1b;
+
+    _area = QPolygonF(points);
 }
 
 RectFigure::RectFigure(QPointF p1, QPointF p2) :
@@ -27,4 +77,9 @@ RectFigure::RectFigure(QPointF p1, QPointF p2) :
 void RectFigure::draw(GraphicsScene *scene)
 {
     scene->addRect(QRectF(_p1, _p2), _pen);
+}
+
+bool RectFigure::inArea(QPointF p)
+{
+    return true; // temp
 }
--- figure.h
+++ figure.h
@@ -9,6 +9,7 @@ public:
     Figure();
     ~Figure(){}
     void virtual draw(GraphicsScene *scene){}
+    bool virtual inArea(QPointF p) { return true; }
 protected:
     QPen _pen;
 };
@@ -19,9 +20,16 @@ public:
     LineFigure(QPointF p1, QPointF p2);
     ~LineFigure(){}
     void draw(GraphicsScene *scene) override;
+    bool inArea(QPointF p) override;
+    void calcArea();
+    QPointF rotatedEndPoint2();
 private:
     QPointF _p1;
     QPointF _p2;
+    QPolygonF _area;
+    const double _offset = 3;
+    double _angle;
+
 };
 
 class RectFigure : public Figure
@@ -30,6 +38,7 @@ public:
     RectFigure(QPointF p1, QPointF p2);
     ~RectFigure(){}
     void draw(GraphicsScene *scene) override;
+    bool inArea(QPointF p) override;
 private:
     QPointF _p1;
     QPointF _p2;
