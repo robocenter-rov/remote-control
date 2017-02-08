765f8ce4940d2ae26e53cfe13675df37473c5be1
iantv <ian.tatiana.v@gmail.com>
2017-02-08 15:03:33 +1000

    Add classes and structs for messages

--- messages.cpp
+++ messages.cpp
@@ -1,2 +1,45 @@
 #include "messages.h"
+#include <QtCore>
 
+QDataStream &operator <<(QDataStream &ds, const MotorsThrustMsg &msg)
+{
+    ds << msg._id
+       << msg._motor1Thrust
+       << msg._motor2Thrust
+       << msg._motor3Thrust
+       << msg._motor4Thrust
+       << msg._motor5Thrust
+       << msg._motor6Thrust;
+    qDebug() << "id: " << msg._id
+             << "\nmotor1Thrust" << msg._motor1Thrust
+             << "\nmotor2Thrust" << msg._motor2Thrust
+             << "\nmotor3Thrust" << msg._motor3Thrust
+             << "\nmotor4Thrust" << msg._motor4Thrust
+             << "\nmotor5Thrust" << msg._motor5Thrust
+             << "\nmotor6Thrust" << msg._motor6Thrust;
+    return ds;
+}
+
+MotorsThrustMsg::MotorsThrustMsg(const MotorsThrustMsg& other)
+{
+    this->_id = other._id;
+    this->_motor1Thrust = other._motor1Thrust;
+    this->_motor2Thrust = other._motor2Thrust;
+    this->_motor3Thrust = other._motor3Thrust;
+    this->_motor4Thrust = other._motor4Thrust;
+    this->_motor5Thrust = other._motor5Thrust;
+    this->_motor6Thrust = other._motor6Thrust;
+}
+
+MotorsThrustMsg::MotorsThrustMsg(
+    int16_t motor1Thrust, int16_t motor2Thrust, int16_t motor3Thrust,
+    int16_t motor4Thrust, int16_t motor5Thrust, int16_t motor6Thrust) :
+    BaseMsg(1)
+{
+    _motor1Thrust = motor1Thrust;
+    _motor2Thrust = motor2Thrust;
+    _motor3Thrust = motor3Thrust;
+    _motor4Thrust = motor4Thrust;
+    _motor5Thrust = motor5Thrust;
+    _motor6Thrust = motor6Thrust;
+}
--- messages.h
+++ messages.h
@@ -2,8 +2,67 @@
 #define MESSAGES_H
 
 #include <cstdint>
+#include <QDataStream>
 
+class BaseMsg
+{
+public:
+    unsigned char _id;
+    BaseMsg() {}
+    BaseMsg(unsigned char id) { _id = id; }
+};
 
+class MotorsThrustMsg : public BaseMsg
+{
+public:
+    MotorsThrustMsg() { _id = 1; }
+    MotorsThrustMsg(const MotorsThrustMsg& other);
+    MotorsThrustMsg(
+        int16_t motor1Thrust, int16_t motor2Thrust, int16_t motor3Thrust,
+        int16_t motor4Thrust, int16_t motor5Thrust, int16_t motor6Thrust);
+    friend QDataStream &operator <<(QDataStream &ds, const MotorsThrustMsg &msg);
+private:
+    int16_t _motor1Thrust;
+    int16_t _motor2Thrust;
+    int16_t _motor3Thrust;
+    int16_t _motor4Thrust;
+    int16_t _motor5Thrust;
+    int16_t _motor6Thrust;
+};
 
+struct SetFlashLightState
+{
+    unsigned char id = 5;
+    int16_t worker_id;
+    uint16_t tag;
+    char state;
+};
+
+struct StartBluetoothReading
+{
+    unsigned char id = 3;
+    int16_t worker_id;
+    uint16_t tag;
+};
+
+struct GetWorkedState
+{
+    unsigned char id = 11;
+    uint16_t worked_id;
+};
+
+struct SetManipulatorPosition
+{
+    unsigned char id = 2;
+    uint16_t hand_pos;
+    uint16_t arm_pos;
+};
+
+struct I2CScan
+{
+    unsigned char id = 17;
+    int16_t worker_id;
+    uint16_t tag;
+};
 
 #endif // MESSAGES_H
