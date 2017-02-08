eac9b102fdf1a3c3d7b9e890de2d8a1c93ecbf37
iantv <ian.tatiana.v@gmail.com>
2017-02-08 15:03:33 +1000

    Remove struct Cmd::AxesValue

--- messages.cpp
+++ messages.cpp
@@ -1,12 +1,2 @@
 #include "messages.h"
 
-namespace Cmd {
-    AxesValue::AxesValue(int16_t a0, int16_t a1, int16_t a2, int16_t a3, int16_t a4)
-        : axis0(a0),
-          axis1(a1),
-          axis2(a2),
-          axis3(a3),
-          axis4(a4)
-    {
-    }
-} //namespace Cmd
--- messages.h
+++ messages.h
@@ -64,14 +64,6 @@ namespace Cmd {
         float angle;    // NOT SOLVED! value in degrees or radians?
     };
 
-    struct AxesValue {
-        AxesValue(int16_t a1, int16_t a2, int16_t a3, int16_t a4 = 0, int16_t a5 = 0);
-        int16_t axis0;
-        int16_t axis1;
-        int16_t axis2;
-        int16_t axis3;
-        int16_t axis4;
-    };
 } // Cmd namespace
 
 namespace Msg {
