07fae7322acc118a0382780e08f8b58a04bfc5d0
iantv <ian.tatiana.v@gmail.com>
2017-02-08 15:03:33 +1000

    Remove unused code of namespace Cmd

--- messages.h
+++ messages.h
@@ -4,64 +4,6 @@
 #include <cstdint>
 
 
-// TO DO: Add constructions, and send methods
-namespace Cmd {
-    /* Low-level commands */
-    /**********************/
-    struct SetDigitalValue
-    {
-        int id;
-        int pin;
-        bool value;
-    };
 
-    struct SetAnalogValue
-    {
-        int id;
-        int pin;
-        int value;
-    };
-
-    /* Mid-level commands */
-    /**********************/
-    struct SetMotorsSpeed
-    {
-        float motor1speed;
-        float motor2speed;
-        float motor3speed;
-        float motor4speed;
-        float motor5speed;
-        float motor6speed;
-    };
-
-    /* High-level commands */
-    /***********************/
-    struct SetDepth
-    {
-        CoordinateSystem coordSystem;
-        float value;
-    };
-
-    struct SetAngleSpeed
-    {
-        CoordinateSystem coord_system;
-        Axis axis;
-        float value;
-    };
-
-    struct SetPosition
-    {
-        CoordinateSystem coordSystem;
-        float x, y, z;
-    };
-
-    struct Rotate
-    {
-        CoordinateSystem coordSystem;
-        Axis axis;
-        float angle;    // NOT SOLVED! value in degrees or radians?
-    };
-
-} // Cmd namespace
 
 #endif // MESSAGES_H
